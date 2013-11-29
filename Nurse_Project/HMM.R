569ff6a9bd6171e61b9f321a3e5c816d85752068
Tao Xu <xutaosjtu@gmail.com>
2013-11-29 17:18:47 +0100

    HMM model modified

--- Nurse_Project/HMM.R
+++ Nurse_Project/HMM.R
@@ -5,18 +5,21 @@ levels(metabo_aggre$SW_Nr)
 obs = list(); i = 1
 for(p in levels(metabo_aggre$SW_Nr)){
   tmp = subset(metabo_aggre, subset=SW_Nr==p&shift=="Tagschicht")
-  obs[[i]] = tmp[,valid_measures[20:32]]
+  obs[[i]] = tmp$Gln
   i = i+1
 }
 names(obs)=levels(metabo_aggre$SW_Nr)
 
 
 obs = sapply(obs, function(x) {sapply(x, function(y) {y[which(is.na(y))] = mean(y, na.rm = T); return(y)})})
+participants = levels(metabo_aggre$SW_Nr)[sapply(obs, function(x)  !is.null(dim(x)))]
+obs = obs[sapply(obs, function(x)  !is.null(dim(x)))]
 
-Res_obs = HMMFit(obs, dis="NORMAL", nStates=2)
-summary(Res_obs)
+## separate the cases and controls
 
+Res_obs_control = HMMFit(obs[control], dis="NORMAL", nStates=3)
+Res_obs_case = HMMFit(obs[case], dis="NORMAL", nStates=3)
 
-state_seq = viterbi(Res_obs, obs)
+state_seq = viterbi(Res_obs, obs[control])
 
-
+require(depmixS4)
